6ff1b80bfbd40c02e7b4f8bb9c0c2503ea06a219
DeveloperFernandito23 <DeveloperFernandito23@users.noreply.github.com>
2023-05-17 16:52:34 +0000

    Deploying to gh-pages from  @ e9e06b973581c133e363f31bc076ac7b2457d5cb 🚀

---
+++ scripts/app.js
@@ -0,0 +1,163 @@
+export function totalHost() {
+    var hosts = document.getElementsByClassName("size");
+    var values = [];
+
+    for (var i = 0; i < hosts.length; i++) {
+        values.push(parseInt(hosts[i].value));
+    }
+
+    return values;
+}
+
+export function takeIp() {
+    var ip = document.getElementById("base-ip-value");
+
+
+    return ip.value;
+}
+
+var alertPlaceholder = document.getElementById('liveAlert');
+
+export function error() {
+    if (alertPlaceholder.innerHTML.length == 0) {
+        alert('<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill" /></svg> ERROR INTRODUCING DATA.', 'danger');
+        var myAlert = document.getElementById('alert-content');
+
+        myAlert.addEventListener('closed.bs.alert', () => {
+            var removeElement = document.getElementById("alert");
+            alertPlaceholder.removeChild(removeElement);
+        });
+    }
+}
+
+function alert(message, type) {
+    var wrapper = document.createElement('div');
+    wrapper.setAttribute("id", "alert");
+    wrapper.innerHTML = '<div class="fade show alert alert-' + type + ' alert-dismissible" role="alert" id="alert-content">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+
+    alertPlaceholder.append(wrapper);
+}
+
+export function drawResult(subnetClassList) {
+
+    var subnetList = JSON.parse(subnetClassList);
+
+    var table = document.getElementsByClassName("table")[0];
+    var thead = table.getElementsByClassName("thead-dark")[0];
+    var tbody = table.getElementsByClassName("tbody")[0];
+    thead.innerHTML = "";
+    tbody.innerHTML = "";
+
+    var rowHead = document.createElement("tr");
+
+    var subnetNameTh = document.createElement("th");
+    subnetNameTh.setAttribute("scope", "col");
+    subnetNameTh.innerHTML = "Subnet Name";
+
+    var subnetRequiredSizeTh = document.createElement("th");
+    subnetRequiredSizeTh.setAttribute("scope", "col");
+    subnetRequiredSizeTh.innerHTML = "Size Required";
+
+    var subnetTotalSizeTh = document.createElement("th");
+    subnetTotalSizeTh.setAttribute("scope", "col");
+    subnetTotalSizeTh.innerHTML = "Total Size";
+
+    var subnetIPBaseTh = document.createElement("th");
+    subnetIPBaseTh.setAttribute("scope", "col");
+    subnetIPBaseTh.innerHTML = "IP Base";
+
+    var subnetRangeTh = document.createElement("th");
+    subnetRangeTh.setAttribute("scope", "col");
+    subnetRangeTh.innerHTML = "IP Range";
+
+    var subnetBroadcastTh = document.createElement("th");
+    subnetBroadcastTh.setAttribute("scope", "col");
+    subnetBroadcastTh.innerHTML = "Broadast";
+
+    var subnetMaskTh = document.createElement("th");
+    subnetMaskTh.setAttribute("scope", "col");
+    subnetMaskTh.innerHTML = "Mask";
+
+    var subnetCIDRTh = document.createElement("th");
+    subnetCIDRTh.setAttribute("scope", "col");
+    subnetCIDRTh.innerHTML = "CIDR";
+
+    table.appendChild(thead);
+    thead.appendChild(rowHead);
+
+    rowHead.appendChild(subnetNameTh);
+    rowHead.appendChild(subnetRequiredSizeTh);
+    rowHead.appendChild(subnetTotalSizeTh);
+    rowHead.appendChild(subnetIPBaseTh);
+    rowHead.appendChild(subnetRangeTh);
+    rowHead.appendChild(subnetBroadcastTh);
+    rowHead.appendChild(subnetMaskTh);
+    rowHead.appendChild(subnetCIDRTh);
+
+    subnetList.forEach(subnet => {
+        var newRow = document.createElement("tr");
+
+        var subnetName = document.createElement("td");
+        subnetName.innerHTML = "";
+        subnetName.innerHTML = subnet.Name;
+
+        var subnetRequiredSize = document.createElement("td");
+        subnetRequiredSize.innerHTML = "";
+        subnetRequiredSize.innerHTML = subnet.Size;
+
+        var subnetTotalSize = document.createElement("td");
+        subnetTotalSize.innerHTML = "";
+        subnetTotalSize.innerHTML = subnet.TotalSize;
+
+        var subnetIPBase = document.createElement("td");
+        subnetIPBase.innerHTML = "";
+        subnetIPBase.innerHTML = subnet.IPBase.join('.');
+
+        var subnetRange = document.createElement("td");
+        subnetRange.innerHTML = "";
+        subnetRange.innerHTML = `${subnet.RangeStart.join('.')} - ${subnet.RangeEnd.join('.')}`;
+
+        var subnetBroadcast = document.createElement("td");
+        subnetBroadcast.innerHTML = "";
+        subnetBroadcast.innerHTML = subnet.Broadcast.join('.');
+
+        var subnetMask = document.createElement("td");
+        subnetMask.innerHTML = "";
+        subnetMask.innerHTML = subnet.Mask.join('.');
+
+        var subnetCIDR = document.createElement("td");
+        subnetCIDR.innerHTML = "";
+        subnetCIDR.innerHTML = `/${subnet.CIDR}`;
+
+        tbody.appendChild(newRow);
+        newRow.appendChild(subnetName);
+        newRow.appendChild(subnetRequiredSize);
+        newRow.appendChild(subnetTotalSize);
+        newRow.appendChild(subnetIPBase);
+        newRow.appendChild(subnetRange);
+        newRow.appendChild(subnetBroadcast);
+        newRow.appendChild(subnetMask);
+        newRow.appendChild(subnetCIDR);
+    });
+
+    table.appendChild(tbody);
+}
+
+export function showMessage() {
+    var icon = '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill" /></svg>'
+    var headerAlert = '<h4 class="alert-heading">INFORMATION MESSAGE!</h4>';
+    var textAlert = '<p>When you press calculate you get an error message?, Check that: First you have filled in all the fields. Then check the syntax that is illustrated below. And finally check if it is possible to do x subnets with those required hosts</p>';
+    var br = '<hr/>'
+    var sintax = '<p> The IP Base: 192.168.2.0/24 <br/> The host number: 10 <br/> The subnets number: 4 <br/> <strong>This is a posible example</strong></p>'
+
+    if (alertPlaceholder.innerHTML.length == 0) {
+        alert(icon + headerAlert + textAlert + br + sintax, 'primary');
+
+        var myAlert = document.getElementById('alert-content');
+
+        myAlert.addEventListener('closed.bs.alert', () => {
+            var removeElement = document.getElementById("alert");
+            alertPlaceholder.removeChild(removeElement);
+        });
+    }
+}
---
+++ service-worker-assets.js
@@ -0,0 +1,233 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-lOCs45frNf+D7me6lKLgUhRCdt4VwHCp9H6Ct+YWzfs=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-rIVQOJ+gHn4DeRSq6Ac3CURS8YBJG3P4CtYPfIuZ\/kk=",
+      "url": "_framework\/dotnet.timezones.blat"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "_framework\/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc=",
+      "url": "_framework\/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-L7sV7NEYP37\/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs=",
+      "url": "_framework\/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-tO5O5YzMTVSaKBboxAqezOQL9ewmupzV2JrB5Rkc8a4=",
+      "url": "_framework\/icudt.dat"
+    },
+    {
+      "hash": "sha256-1lQUgCrWgNv1EMblLxCKfhBpxFwgp3kGxjZF0pPQ6j4=",
+      "url": "_framework\/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-12Q6zEA2eL\/wuP4aqHG5oalCZmtIsehmjGDHKuRfV2I=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.dll"
+    },
+    {
+      "hash": "sha256-wUXuPOatzqqnn6dWB8wmWeZMhC3A7c\/Gaw6nwAoqeds=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Web.dll"
+    },
+    {
+      "hash": "sha256-IiCeDD2\/cESvpd281cmPUvaLlj5RlX\/Gy2c0FzTfcec=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-X\/f4fDl2cuIRXeWHhK\/f2UqQbFioD+RU4a4CEh0zrrQ=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-DBOKSPriP2JDxVbbWrLXyD3K4\/x3RBifNBWk\/q1I39M=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.dll"
+    },
+    {
+      "hash": "sha256-Q5AqJneA2TZnzC0IYzBx6j\/tHRhWAeMbpH3BsV7KgWg=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Json.dll"
+    },
+    {
+      "hash": "sha256-7ni+nO8H2QxkXyRXRureFzJc0Tr5aT45TAYcdlgpmwQ=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-qi0kE7rp0kdsNqdL6DyPZEeimjUGvcLT4iWQX0YnRus=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.dll"
+    },
+    {
+      "hash": "sha256-qZdDWEYBno4I8vPywu9Fyn5LLwSVquG8Y7JzRiWgFEA=",
+      "url": "_framework\/Microsoft.Extensions.Logging.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-RJE660TEC1wm3MyL1EXQfTQB1qK+JWy1efVUxI8nNUo=",
+      "url": "_framework\/Microsoft.Extensions.Logging.dll"
+    },
+    {
+      "hash": "sha256-WPBQk7rt2p4aMEo2pTp1sBpsiWdE8MmWuIYq+zl1ceo=",
+      "url": "_framework\/Microsoft.Extensions.Options.dll"
+    },
+    {
+      "hash": "sha256-eXvGx2jcjpTPEJoAHBsW\/VuMPbNyyU+AsuhPmkzSSRY=",
+      "url": "_framework\/Microsoft.Extensions.Primitives.dll"
+    },
+    {
+      "hash": "sha256-KK2enRi1Ou0q9Fotg\/0jFC2B+uiKQUpPb7VlPVdWBw4=",
+      "url": "_framework\/Microsoft.JSInterop.dll"
+    },
+    {
+      "hash": "sha256-3+dHa65PgCYOsUMlsJSDYPxt294+nWZxeMbopoWb+fA=",
+      "url": "_framework\/Microsoft.JSInterop.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-mDanEYPJrXpvJ2qUAe2G8di67lA2fMrVLduE2rxsFaE=",
+      "url": "_framework\/Subnetting-Calculator.dll"
+    },
+    {
+      "hash": "sha256-MIAyCKfDjC7Lr\/pqpMU9F25ZAN3Tzkue40mEwEf7+hE=",
+      "url": "_framework\/System.Collections.Concurrent.dll"
+    },
+    {
+      "hash": "sha256-MC0YSS3BHjBKiknf5xrFk8cuh\/pIX60TqP\/2nhiNVVM=",
+      "url": "_framework\/System.Collections.dll"
+    },
+    {
+      "hash": "sha256-nDL4uAnV8XjBp7HMR8402tCIjKLnxen\/fQEcevUyqXs=",
+      "url": "_framework\/System.ComponentModel.dll"
+    },
+    {
+      "hash": "sha256-NoLTriD91aOdChM3reTvKqIRpGlSXJBVwibs0Lw5kzU=",
+      "url": "_framework\/System.Linq.dll"
+    },
+    {
+      "hash": "sha256-JXImOhG2oDuiRYqFJcwoBXPI04+Wgv9+8NqWpM0\/Mxo=",
+      "url": "_framework\/System.Memory.dll"
+    },
+    {
+      "hash": "sha256-yrCNNkcTQEessfBuAqsQ9qgBmG8PMBRHkbURq27uMRE=",
+      "url": "_framework\/System.Net.Http.dll"
+    },
+    {
+      "hash": "sha256-0HTGrEBoWDqqIOwcbWPpyUBIfLagz21\/yavULaMDDts=",
+      "url": "_framework\/System.Net.Primitives.dll"
+    },
+    {
+      "hash": "sha256-1tZSuuaPx80yUvdi2lBDkaCSUAgZHRVa4ZhHd60wBew=",
+      "url": "_framework\/System.Private.CoreLib.dll"
+    },
+    {
+      "hash": "sha256-3I8vt9q7HSkVDJJLGexUuIeBJne2bh0SMTAkiXt3GvQ=",
+      "url": "_framework\/System.Private.Uri.dll"
+    },
+    {
+      "hash": "sha256-JIB5Zz5YbqtuqQ3p\/bsx02e8sEQD6UA12RRM153oSu4=",
+      "url": "_framework\/System.Runtime.dll"
+    },
+    {
+      "hash": "sha256-a18FjgMkDdKhrXsStntZnVnDQtWqc+6O36uGgHrst6k=",
+      "url": "_framework\/System.Runtime.InteropServices.JavaScript.dll"
+    },
+    {
+      "hash": "sha256-dBAu8sl6qcNjD5aAePrxOnRjOf3o+OZvh4IohTnfsNE=",
+      "url": "_framework\/System.Text.Encodings.Web.dll"
+    },
+    {
+      "hash": "sha256-64Ps9jA+1YATEUvOT6K\/2mXkEhi\/4MoSsYFIKPDBQL8=",
+      "url": "_framework\/System.Text.Json.dll"
+    },
+    {
+      "hash": "sha256-SzoGrb2TwjA3ED9Y41QHLzqQBU7p92hA+eOu\/RIlqxQ=",
+      "url": "_framework\/System.Text.RegularExpressions.dll"
+    },
+    {
+      "hash": "sha256-DeOmWktIeTo5QhRJtSOcBx\/ZaQR6ExUgwmDx+uWtAxk=",
+      "url": "_framework\/dotnet.7.0.5.pfw0e7axea.js"
+    },
+    {
+      "hash": "sha256-dT52UoGlCKE1vzrEcQYNywKVXwYUyyKOLCCWhHd2zpw=",
+      "url": "_framework\/dotnet.wasm"
+    },
+    {
+      "hash": "sha256-\/Ap\/vKsrWPVOOrQ+5sZFrnkGkp5SLYqp7q2EJxPBHCg=",
+      "url": "Subnetting-Calculator.styles.css"
+    },
+    {
+      "hash": "sha256-10KEeWxsgry5D9LUZc0K86lpIg9RcWdyyBB5sXq6Ypk=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-z8OR40MowJ8GgK6P89Y+hiJK5+cclzFHzLhFQLL92bg=",
+      "url": "css\/bootstrap\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-gBwg2tmA0Ci2u54gMF1jNCVku6vznarkLS6D76htNNQ=",
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-+Q44zfEaCMmXduni5Td+IgCbk8sSUQwES2nWs+KKQz0=",
+      "url": "css\/open-iconic\/FONT-LICENSE"
+    },
+    {
+      "hash": "sha256-BJ\/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=",
+      "url": "css\/open-iconic\/font\/css\/open-iconic-bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-OK3poGPgzKI2NzNgP07XMbJa3Dz6USoUh\/chSkSvQpc=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.eot"
+    },
+    {
+      "hash": "sha256-sDUtuZAEzWZyv\/U1xl\/9D3ehyU69JE+FvAcu5HQ+\/a0=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.otf"
+    },
+    {
+      "hash": "sha256-oUpLdS+SoLJFwf4bzA3iKD7TCm66oLkTpAQlVJ2s1wc=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.svg"
+    },
+    {
+      "hash": "sha256-p+RP8CV3vRK1YbIkNzq3rPo1jyETPnR07ULb+HVYL8w=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.ttf"
+    },
+    {
+      "hash": "sha256-cZPqVlRJfSNW0KaQ4+UPOXZ\/v\/QzXlejRDwUNdZIofI=",
+      "url": "css\/open-iconic\/font\/fonts\/open-iconic.woff"
+    },
+    {
+      "hash": "sha256-s\/Is6Ey6jfNAEfXUIOyHrXXX+RcA8hzchYnuOIWUMl4=",
+      "url": "css\/open-iconic\/ICON-LICENSE"
+    },
+    {
+      "hash": "sha256-f7rXj2KSx8rOrKHjkkyN+tLJV5mfHEDVLGIZvcj\/hgg=",
+      "url": "css\/open-iconic\/README.md"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk\/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-YbmtEcm2T4lDmAaKLO8CSHNfqeMPqG5SFaOhs/EGwmc=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-EIuiHnrVJxJfAs8Na657tF96xBD02pozdV\/Lhj749EA=",
+      "url": "manifest.json"
+    },
+    {
+      "hash": "sha256-9tgW2gY\/qJ1LhRH5WqutBVkXZf6sJ+5Nl0k4woerOCA=",
+      "url": "sample-data\/favicon.png"
+    },
+    {
+      "hash": "sha256-cWvMNY4PUzo45JPgw2Nllgge1eaQbSG8xFyus0vr2jc=",
+      "url": "scripts\/app.js"
+    }
+  ],
+  "version": "q2g7x0tj"
+};
